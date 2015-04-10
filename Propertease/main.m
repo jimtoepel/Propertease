ce89fff152e5802de28ace5460883907b79821ec
jimtoepel <jim.toepel@gmail.com>
2015-04-09 20:41:04 -0700

    Not quite working. Come back at it tomorrow.

--- Propertease/main.m
+++ Propertease/main.m
@@ -10,8 +10,40 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+/*      
+ 
+        Array           ????
+        Dictionary      System
+        String          Name
+        Data
+        Date            Started
+        Integer         Times Played
+        float           Hours Played
+        boolean         Finished?
+        
+        
+    */
+        NSMutableArray *games = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *game;
+        
+        game = [NSMutableDictionary dictionary];
+        [game setObject:@"Bloodborne"
+                 forKey:@"title"];
+        [game setObject:@"M"
+                 forKey:@"rating"];
+        [games addObject:game];
+        
+        [games writeToFile:@"/tmp/games.plist"
+                atomically:YES];
+        
+        
+        NSArray *gameList = [NSArray arrayWithContentsOfFile:@"/tmp/games.plist"];
+        
+        for (NSDictionary *d in gameList) {
+            NSLog(@"I have spent %@ hours playing %@", [d objectForKey:@"hoursPlayed"],[d objectForKey:@"title"]);
+        }
+        
     }
     return 0;
 }
