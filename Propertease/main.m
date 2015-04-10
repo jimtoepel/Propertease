8ed94ba0edf59b51339fee95b2036e306bdb8e46
jimtoepel <jim.toepel@gmail.com>
2015-04-09 20:12:21 -0700

    Initial Commit

---
+++ Propertease/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Propertease
+//
+//  Created by Jim Toepel on 4/9/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
